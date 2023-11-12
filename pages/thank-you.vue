2bf185ddf3678235d96f15cc6fb6ff8f70e215e1
Avi E Koenig <darkmift@gmail.com>
2023-11-12 11:54:48 -0300

    [main] added ty page

---
+++ pages/thank-you.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" class="d-flex flex-column align-center">
+        <v-icon color="red" size="100">mdi-heart</v-icon>
+        <v-card-text class="text-center">
+          <h2 class="mb-4">{{ $t('thankYou') }}<span>!</span></h2>
+          <p>
+            {{ $t('messageReceived') }}
+          </p>
+        </v-card-text>
+        <v-btn to="/form" variant="elevated" color="primary" nuxt> {{ $t('fileAnotherRequest') }}</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+const i18n = useI18n({
+  useScope: 'global',
+  locale: 'he',
+  messages: {
+    en: {
+      thankYou: 'Thank You',
+      messageReceived: 'Your message has been received, we will get back to you soon.',
+      fileAnotherRequest: 'To file another request, click here',
+    },
+    fr: {
+      thankYou: 'Merci',
+      messageReceived: 'Votre message a été reçu, nous vous répondrons bientôt.',
+      fileAnotherRequest: 'Pour déposer une autre demande, cliquez ici',
+    },
+    he: {
+      thankYou: 'תודה רבה',
+      messageReceived: 'בקשתך נשלחה אלינו, נחזור אליך בהקדם',
+      fileAnotherRequest: 'להגשת בקשה נוספת לחץ כאן',
+    },
+  },
+});
+</script>
