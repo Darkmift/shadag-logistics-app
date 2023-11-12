5d4f4c149e4bb1a37338b2210791a3b0c7c4e547
Avi E Koenig <darkmift@gmail.com>
2023-11-12 11:57:10 -0300

    [main] responsivness fix

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -3,10 +3,10 @@
     <v-app-bar-nav-icon variant="text" @click.stop="drawerStore.toggleDrawer"></v-app-bar-nav-icon>
     <v-toolbar-title>{{ $t('appName') }}</v-toolbar-title>
     <v-spacer></v-spacer>
-    <div class="pt-5 mx-5" style="width: 200px">
+    <div class="pt-5 mx-5 hidden-sm-and-down" style="width: 200px">
       <SelectLanguage />
     </div>
-    <dark-mode-btn />
+    <dark-mode-btn class="hidden-sm-and-down" />
   </v-app-bar>
 </template>
 
